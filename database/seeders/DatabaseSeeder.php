0d5be1c610013c76714c2ddaabd3a2005448d830
Lucas Yang <yangchenshin77@gmail.com>
2023-06-06 19:41:51 +0800

    Remove seed history data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\History;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,27 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $user_1 = User::create([
+        User::create([
             'name' => '舊電腦',
         ]);
         User::create([
             'name' => 'HP 銀色電腦',
-        ]);
-
-        History::create([
-            'url' => 'https://laravel.com/',
-            'hostname' => 'laravel.com',
-            'user_id' => $user_1->id,
-        ]);
-        History::create([
-            'url' => 'https://www.youtube.com/',
-            'hostname' => 'www.youtube.com',
-            'user_id' => $user_1->id,
-        ]);
-        History::create([
-            'url' => 'https://translate.google.com.tw/',
-            'hostname' => 'translate.google.com.tw',
-            'user_id' => $user_1->id,
         ]);
     }
 }
